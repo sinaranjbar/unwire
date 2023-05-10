4534c53aafb141001f21f66e77b1e4e7e3c07874
Sina <sinaranjbar90@gmail.com>
2023-05-10 19:15:18 +0300

    :tada: Initial commit.

---
+++ unwire/unwire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unwire
+//
+//  Created by Sina Ranjbar on 5/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ unwire/unwireUITests/unwireUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  unwireUITestsLaunchTests.swift
+//  unwireUITests
+//
+//  Created by Sina Ranjbar on 5/10/23.
+//
+
+import XCTest
+
+final class unwireUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
